8ab7cac5c0a0cd94dcc71ef76f5578bca44a6514
Gyömbér Judit <gyomberjudit@gmail.com>
2023-04-15 21:40:52 +0200

    add AboutPageTest

---
+++ src/test/java/org/example/portio/AboutPageTest.java
@@ -0,0 +1,27 @@
+package org.example.portio;
+
+import io.qameta.allure.Epic;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AboutPageTest extends BaseTest{
+    @Epic("Technical functionalities")
+    @Story("Navigation")
+    @Test
+    public void testButtonHireMe() {
+        testLinkAbout();
+        aboutPage.clickOnHireMe();
+        Assertions.assertTrue(contactPage.isContactMeTextDisplayed());
+        Assertions.assertEquals(Pages.CONTACT_PAGE.getUrl(), driver.getCurrentUrl());
+    }
+    @Epic("Technical functionalities")
+    @Story("FileDownloading")
+    @Test
+    public void testFileDownloaded() {
+        String filename = "CV";
+        testLinkAbout();
+        aboutPage.clickDownloadCV();
+        Assertions.assertTrue(aboutPage.isFileExist(filename));
+    }
+}
