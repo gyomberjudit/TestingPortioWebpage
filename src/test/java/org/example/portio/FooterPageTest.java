e7e33a125a8682b2bda428e2e22170bb061316f8
Gyömbér Judit <gyomberjudit@gmail.com>
2023-04-16 02:12:10 +0200

    add FooterPageTest.java

---
+++ src/test/java/org/example/portio/FooterPageTest.java
@@ -0,0 +1,108 @@
+package org.example.portio;
+
+import io.qameta.allure.Allure;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.ByteArrayInputStream;
+
+public class FooterPageTest extends BaseTest{
+    @Epic("Technical functionalities")
+    @Story("Navigating")
+    @Test
+    public void testGetInTouchButton() {
+        testLinkContact();
+        footerPage.clickGetINTouchButton();
+        Assertions.assertTrue(contactPage.isContactMeTextDisplayed());
+    }
+    @Epic("Technical functionalities")
+    @Story("Navigating")
+    @Test
+    public void testAboutMeLink() {
+        testLinkContact();
+        footerPage.clickLinkAboutMe();
+        Allure.addAttachment("Failed navigation to About Me", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+        Assertions.assertFalse(footerPage.isFileNotFound());
+    }
+    @Epic("Technical functionalities")
+    @Story("Navigating")
+    @Test
+    public void testFrequentlyAskedQuestionLink() {
+        testLinkContact();
+        footerPage.clickLinkFAQ();
+        Allure.addAttachment("Failed navigation to FAQ", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+        Assertions.assertFalse(loginPage.isLoginButtonDisplayed());
+    }
+    @Epic("Technical functionalities")
+    @Story("Navigating")
+    @Test
+    public void testPrivacyAndPolicyLink() {
+        testLinkContact();
+        footerPage.clickLinkPrivacyAndPolicy();
+        Allure.addAttachment("Failed navigation to Privacy and Policy", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+        Assertions.assertFalse(loginPage.isLoginButtonDisplayed());
+    }
+    @Epic("Technical functionalities")
+    @Story("Navigating")
+    @Test
+    public void testLatestArticleLink() {
+        testLinkContact();
+        footerPage.clickLatestArticle();
+        Allure.addAttachment("Failed navigation to Latest Article", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+        Assertions.assertFalse(loginPage.isLoginButtonDisplayed());
+    }
+    @Epic("Technical functionalities")
+    @Story("Navigating")
+    @Test
+    public void testTermsAndConditionsLink() throws InterruptedException {
+        testLinkContact();
+        footerPage.clickTermsAndConditions();
+        Allure.addAttachment("Successful navigation to Terms and Conditions", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+        Assertions.assertTrue(footerPage.isTermsAndConditionsTextDisplayed());
+        Assertions.assertEquals(Pages.TERMS_AND_CONDITIONS.getUrl(), driver.getCurrentUrl());
+    }
+    @Epic("Technical functionalities")
+    @Story("Navigating")
+    @Test
+    public void testFacebookLink() throws InterruptedException {
+        testLinkContact();
+        footerPage.clickIconFacebbok();
+        String actual = footerPage.getChildWindowUrl();
+        Allure.addAttachment("Successful navigation to Facebook", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+        Assertions.assertEquals(Pages.FACEBOOK.getUrl(), actual);
+    }
+    @Epic("Technical functionalities")
+    @Story("Navigating")
+    @Test
+    public void testLinkedinLink() throws InterruptedException {
+        testLinkContact();
+        footerPage.clickIconLinkedin();
+        String actual = footerPage.getChildWindowUrl();
+        Allure.addAttachment("Successful navigation to Linkedin", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+        Assertions.assertEquals(Pages.LINKEDIN.getUrl(), actual);
+    }
+    @Epic("Technical functionalities")
+    @Story("Navigating")
+    @Test
+    public void testPinterestLink() throws InterruptedException {
+        testLinkContact();
+        footerPage.clickIconPinterest();
+        String actual = footerPage.getChildWindowUrl();
+        Allure.addAttachment("Successful navigation to Pinterest", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+        Assertions.assertEquals(Pages.PINTEREST.getUrl(), actual);
+    }
+    @Epic("Technical functionalities")
+    @Story("Navigating")
+    @Test
+    public void testTwitterLink() throws InterruptedException {
+        testLinkContact();
+        footerPage.clickIconTwitter();
+        String actual = footerPage.getChildWindowUrl();
+        Allure.addAttachment("Successful navigation to Twitter", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+        Assertions.assertEquals(Pages.TWITTER.getUrl(), actual);
+    }
+}
