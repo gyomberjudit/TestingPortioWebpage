9ad65d6b15f2f85e713f179ca89959ef95cef12b
Gyömbér Judit <gyomberjudit@gmail.com>
2023-04-16 16:17:15 +0200

    add BaseTest.java

--- src/test/java/org/example/portio/BaseTest.java
+++ src/test/java/org/example/portio/BaseTest.java
@@ -1,11 +1,33 @@
 package org.example.portio;
 
+import io.qameta.allure.Allure;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.ByteArrayInputStream;
 
 public class BaseTest extends TestEnvironment{
+    @Epic("Technical functionalities")
+    @Story("Profile")
+    @Test
+    public void testSetProfile() {
+        String username = "teszteszter";
+        String password = "teszt";
+        String name ="Teszt Eszti";
+        String bio ="female";
+        String phone ="06301112233";
+        registerPage.registration();
+        registerPage.clickLoginButton();
+        loginPage.login2(username, password);
+        Assertions.assertTrue(homePage.isPortioLogoDisplayed());
+        homePage.clickProfile();
+        profilePage.setProfile(name, bio, phone);
+        Assertions.assertEquals("Profile Edited!", profilePage.getProfileMessage());
+    }
     @Epic("Technical functionalities")
     @Story("Navigating")
     @Test
@@ -29,6 +51,16 @@ public class BaseTest extends TestEnvironment{
         loginPage.login();
         homePage.clickOnLinkWork();
         Assertions.assertTrue(workPage.isWorkPageTextDisplayed());
+    }
+    @Epic("Technical functionalities")
+    @Story("Navigation")
+    @Test
+    public void testLinkCaseStudyOne() {
+        testLinkWork();
+        workPage.clickLinkCaseStudyOne();
+        Allure.addAttachment("Case Study One Page", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+        Assertions.assertTrue(caseStudyPage.isCaseStudyTextVisible());
+        Assertions.assertEquals(Pages.CASE_STUDY_PAGE.getUrl(), driver.getCurrentUrl());
     }
     @Epic("Technical functionalities")
     @Story("Navigation")
@@ -57,7 +89,8 @@ public class BaseTest extends TestEnvironment{
     @Epic("Technical functionalities")
     @Story("Navigating")
     @Test
-    public void testButtonSeeAllPosts() {
+    public void
+    testButtonSeeAllPosts() {
         testLinkBlog();
         blogPage.clickButtonSeeAllPosts();
         Assertions.assertTrue(blogPage2.isBlogPage2TextDisplayed());
