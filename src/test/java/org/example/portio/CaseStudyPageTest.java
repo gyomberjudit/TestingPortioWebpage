9c78bdaf22ac5c755ba34aafc1b744f3c742c95c
Gyömbér Judit <gyomberjudit@gmail.com>
2023-04-18 14:09:52 +0200

    add refresh CaseStudyPageTest.java

--- src/test/java/org/example/portio/CaseStudyPageTest.java
+++ src/test/java/org/example/portio/CaseStudyPageTest.java
@@ -1,9 +1,9 @@
 package org.example.portio;
 
 import io.qameta.allure.Allure;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Story;
+import io.qameta.allure.Description;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -11,8 +11,8 @@ import org.openqa.selenium.TakesScreenshot;
 import java.io.ByteArrayInputStream;
 
 public class CaseStudyPageTest extends TestEnvironment{
-    @Epic("Technical functionalities")
-    @Story("Navigation")
+    @DisplayName("Test Case Study One link")
+    @Description("Scroll down to Recent Works and click on the first work, CAse Study One")
     @Test
     public void testLinkCaseStudyOne() {
         loginPage.login();
@@ -22,18 +22,9 @@ public class CaseStudyPageTest extends TestEnvironment{
         Assertions.assertTrue(caseStudyPage.isCaseStudyTextVisible());
         Assertions.assertEquals(Pages.CASE_STUDY_PAGE.getUrl(), driver.getCurrentUrl());
     }
-    @Epic("Technical functionalities")
-    @Story("Navigation")
-    @Test
-    public void testClickOnNextArrow() {
-        loginPage.login();
-        homePage.clickOnLinkWork();
-        workPage.clickLinkCaseStudyOne();
-        caseStudyPage.clickOnNextArrow();
-        Assertions.assertEquals(Pages.EVENT_APP_CASE_STUDY.getUrl(), driver.getCurrentUrl());
-    }
-    @Epic("Technical functionalities")
-    @Story("Navigation")
+    //Pagination using do-while, making screenshot of every page
+    @DisplayName("Pagination with case study links")
+    @Description("Click on the first recent work (Case Study One) and then click case study links one after each other, at the buttom of the page")
     @Test
     public void testClickOnNextCase() {
         loginPage.login();
@@ -45,5 +36,15 @@ public class CaseStudyPageTest extends TestEnvironment{
             Allure.addAttachment("Next Case", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
         } while (!caseStudyPage.isLinkNextCaseDisplayed());
         Assertions.assertTrue(recipeAppPage.isRecipeAppTextDisplayed());
+    }
+    @DisplayName("Pagination with next arrow")
+    @Description("Failed pagination, next-arrow doesn't work")
+    @Test
+    public void testClickOnNextArrow() {
+        loginPage.login();
+        homePage.clickOnLinkWork();
+        workPage.clickLinkCaseStudyOne();
+        caseStudyPage.clickOnNextArrow();
+        Assertions.assertEquals(Pages.EVENT_APP_CASE_STUDY.getUrl(), driver.getCurrentUrl());
     }
 }
