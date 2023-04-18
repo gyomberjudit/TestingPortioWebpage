abb3d23baa42af35d53fab7efbed1952cd5cf1f0
Gyömbér Judit <gyomberjudit@gmail.com>
2023-04-18 18:00:24 +0200

    add annotations

--- src/test/java/org/example/portio/WorkPageTest.java
+++ src/test/java/org/example/portio/WorkPageTest.java
@@ -1,14 +1,9 @@
 package org.example.portio;
 
-import io.qameta.allure.Allure;
 import io.qameta.allure.Description;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-
-import java.io.ByteArrayInputStream;
 
 public class WorkPageTest extends TestEnvironment{
     @DisplayName("Number of Recent Works")
@@ -26,7 +21,6 @@ public class WorkPageTest extends TestEnvironment{
         String[] text = {"Case Study One", "Event App Case Study", "UX Case Study for Agriculture App", "Recipe App Ux Study"};
         loginPage.login();
         homePage.clickOnLinkWork();
-        Allure.addAttachment("Content of portfolio", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
         Assertions.assertArrayEquals(text, workPage.contentOfPortfolio());
     }
 }
