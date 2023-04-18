a6239bd6a9253c02af095a491b2524da01585ec9
Gyömbér Judit <gyomberjudit@gmail.com>
2023-04-18 14:12:06 +0200

    add refresh FooterPage.java

--- src/main/java/org/example/portio/FooterPage.java
+++ src/main/java/org/example/portio/FooterPage.java
@@ -2,29 +2,16 @@ package org.example.portio;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 import java.util.Set;
 
 public class FooterPage extends  BasePage{
 
 //locate elements for homePage
-    private final By FOOTER_PAGE_TEXT = By.xpath("(//*[contains(@class, \"text-light\")]/h2)[2]");
-    private final By BUTTON_GET_IN_TOUCH = By.xpath("//*[@class=\"footer__cta_action\"]/a");
-    private final By LINK_ABOUT_ME = By.xpath("//*[@class=\"unstyle-list small\"]/li[1]/a");
-    private final By FILE_NOT_FOUND = By.xpath("/html/body/div/h1");
     private final By LINK_FAQ = By.xpath("//*[@class=\"unstyle-list small\"]/li[2]/a");
     private final By LINK_PRIVACY_AND_POLICY = By.xpath("//*[@class=\"unstyle-list small\"]/li[3]/a");
-    private final By LINK_LATEST_ARTICLE = By.xpath("//*[@class=\"unstyle-list small\"]/li[4]/a");
-    private final By LINK_TERMS_AND_CONDITIONS = By.xpath("//*[@class=\"footer__footer_copy text-light\"]/a");
     private final By ICON_FACEBOOK = By.xpath("//*[@class=\"unstyle-list\"]/li[1]/a");
     private final By ICON_LINKEDIN = By.xpath("//*[@class=\"unstyle-list\"]/li[2]/a");
-    private final By ICON_PINTEREST = By.xpath("//*[@class=\"unstyle-list\"]/li[3]/a");
-    private final By ICON_TWITTER = By.xpath("//*[@class=\"unstyle-list\"]/li[4]/a");
-    private final By TEXT_TERMS_AND_CONDITIONS = By.xpath("/html/body/div/h1");
 
 
 //constructor
@@ -34,26 +21,17 @@ public class FooterPage extends  BasePage{
 
 
 //methods
-    public void clickGetINTouchButton() {
-        moveToElement(BUTTON_GET_IN_TOUCH);
+    public void clickLinkFAQ() throws InterruptedException {
+        scrollToElement(LINK_FAQ);
+        driver.findElement(LINK_FAQ).click();
     }
-    public void clickLinkAboutMe() {
-        moveToElement(LINK_ABOUT_ME);
+    public void clickLinkPrivacyAndPolicy() throws InterruptedException {
+        scrollToElement(LINK_PRIVACY_AND_POLICY);
+        driver.findElement(LINK_PRIVACY_AND_POLICY).click();
     }
-    public void clickLinkFAQ() {
-        moveToElement(LINK_FAQ);
-    }
-    public void clickLinkPrivacyAndPolicy() {
-        moveToElement(LINK_PRIVACY_AND_POLICY);
-    }
-    public void clickLatestArticle() {
-        moveToElement(LINK_LATEST_ARTICLE);
-    }
-    public void clickTermsAndConditions() {
-        moveToElement(LINK_TERMS_AND_CONDITIONS);
-    }
-    public void clickIconFacebbok() {
-       moveToElement(ICON_FACEBOOK);
+    public void clickIconFacebbok() throws InterruptedException {
+        scrollToElement(ICON_FACEBOOK);
+        driver.findElement(ICON_FACEBOOK).click();
     }
     public String getChildWindowUrl() throws InterruptedException {
         String parentWindow = driver.getWindowHandle();
@@ -71,28 +49,8 @@ public class FooterPage extends  BasePage{
         driver.switchTo().window(parentWindow);
         return url.toString();
     }
-    public void clickIconLinkedin() {
-        moveToElement(ICON_LINKEDIN);
-    }
-    public void clickIconPinterest() {
-        moveToElement(ICON_PINTEREST);
-    }
-    public void clickIconTwitter() {
-        moveToElement(ICON_TWITTER);
-    }
-
-//methods for assertion
-    public boolean isFooterPageTextDisplayed() {
-        return driver.findElement(FOOTER_PAGE_TEXT).isDisplayed();
-    }
-    public boolean isTermsAndConditionsTextDisplayed() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement text = wait.until(ExpectedConditions.elementToBeClickable(TEXT_TERMS_AND_CONDITIONS));
-        return text.isDisplayed();
-    }
-    public boolean isFileNotFound() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement text = wait.until(ExpectedConditions.elementToBeClickable(FILE_NOT_FOUND));
-        return text.isDisplayed();
+    public void clickIconLinkedin() throws InterruptedException {
+        scrollToElement(ICON_LINKEDIN);
+        driver.findElement(ICON_LINKEDIN).click();
     }
 }
