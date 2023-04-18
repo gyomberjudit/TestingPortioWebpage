6d5c91e02811141122e3681e6316205295ffaa34
Gyömbér Judit <gyomberjudit@gmail.com>
2023-04-18 16:52:23 +0200

    refresh WorkPageTest.java

--- src/test/java/org/example/portio/WorkPageTest.java
+++ src/test/java/org/example/portio/WorkPageTest.java
@@ -1,9 +1,9 @@
 package org.example.portio;
 
 import io.qameta.allure.Allure;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Story;
+import io.qameta.allure.Description;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -11,24 +11,16 @@ import org.openqa.selenium.TakesScreenshot;
 import java.io.ByteArrayInputStream;
 
 public class WorkPageTest extends TestEnvironment{
-
-    @Epic("Content functionalities")
-    @Story("Portfolio")
+    @DisplayName("Number of Recent Works")
+    @Description("Count numer of Recent Works")
     @Test
     public void testQuantityOfPortfolio() {
         loginPage.login();
         homePage.clickOnLinkWork();
         Assertions.assertEquals(4, workPage.quantityOfPortfolio());
-    }@Epic("Content functionalities")
-    @Story("Portfolio")
-    @Test
-    public void testQuantityOfPortfolio2() {
-        loginPage.login();
-        homePage.clickOnLinkWork();
-        Assertions.assertEquals(4, workPage.quantityOfPortfolio2());
     }
-    @Epic("Content functionalities")
-    @Story("Portfolio")
+    @DisplayName("Titles of Recent Works")
+    @Description("Get all the titles of Recent Works in a String array")
     @Test
     public void testContentOfPortfolio() {
         String[] text = {"Case Study One", "Event App Case Study", "UX Case Study for Agriculture App", "Recipe App Ux Study"};
